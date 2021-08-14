eec628cd89311b66434e33b2067683c635c58e83
onesun14 <88918794+onesun14@users.noreply.github.com>
2021-08-14 13:30:27 +0900

    Add files via upload

---
+++ kk.py
@@ -0,0 +1,19 @@
+a = []
+for i in range(100):
+    a.append(i)
+if i == 99:
+    print(a)
+
+c = []
+d = 0
+b = 0
+import random
+for i in range(100):
+    b = random.randrange(1, 100)
+    c.append(b)
+
+
+
+
+if i == 99:
+    print(d)
