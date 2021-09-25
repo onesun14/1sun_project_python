95167134e5383f6945d9c2d2285358181f472e96
onesun14 <88918794+onesun14@users.noreply.github.com>
2021-09-25 13:39:19 +0900

    Add files via upload

--- 12.1.py
+++ 12.1.py
@@ -4,29 +4,29 @@ a = 1
 b = 50
 c = 100
 d = []
-for i in range (100):
+for i in range(100):
     d.append(i+1)
 if 50 >= num:
-    for i in range (100):
+    for i in range(100):
         e = a + b
         f = e // 2
         if f == num:
-            print(i+1)
+            print('{}번 만에 찾았습니다!'.format(i+1))
             break
         elif f < num:
-            b = f
-        elif f > num:
             a = f
+        elif f > num:
+            b = f
 if 50 <= num:
     for i in range (100):
         e = c + b
         f = e // 2
         if f == num:
-            print(i+1)
+            print('{}번 만에 찾았습니다!'.format(i+1))
             break
         elif f < num:
             b = f
         elif f > num:
             c = f
 
-print(d[f-1])
+print('당신이 찾는 숫자는 {}'.format(d[f-1]))
--- 14.py
+++ 14.py
@@ -1,13 +1,20 @@
-id_list = ['aaaaaaaa', 'bbbbbbbb', 'cccccccc']
-pwd_list = ['A!', 'B@', 'A!']
-count = 0
-temp_id = input("Enter the your id: ")
-while True:
-    for i in range(len(id_list)):
-        if temp_id != id_list[i]:
-            count = 1
-            break
-    if count >= 1:
-        
+import random
+def cal_sum(x, y):
+    z = x+y
+    #함수를 종료
+    #값을 변환한다.
+    return z
+def random_list():
+    a = []
+    for i in range(10):
+        random_num = random.randrange(1, 100)
+        a.append(random_num)
+    return a
+def max_value(a):
+    max_value = a[0]
+    for i in range(len(a)):
+        if max_value < a[i]:
+            max_value = a[i]
+    return max_value
 
-temp_pwd = input("Enter the your pwd: ")
+print(max_value(random_list()))
