9b3dde788c63aa5666e8be3a59cab27aaecdafb2
onesun14 <88918794+onesun14@users.noreply.github.com>
2022-02-05 13:32:12 +0900

    Update setting.py

--- game/pygame/setting.py
+++ game/pygame/setting.py
@@ -9,7 +9,27 @@ score = 0
 screen = pygame.display.set_mode([display_width, display_height])
 nemoImg = pygame.image.load('./nemo.png')
 knemoImg = pygame.image.load('./knemo.png')
-#background = pygame.image.load('./space.png')
+fnemoImg = pygame.image.load('./fnemo.png')
+background = pygame.image.load('./space.png')
+title = pygame.image.load('./title.png')
+title_back = pygame.image.load('./title_back.png')
+
+# 버튼의 밝은 색상
+color_light = (170, 170, 170)
+# 버튼의 어두운 색상
+color_dark = (100, 100, 100)
+#검은색
+black = (0, 0, 0)
+#하얀색
+white = (255, 255, 255)
+#빨간색
+red = (255, 0, 0)
+#초록색
+green = (0, 255, 0)
+#파란색
+blue = (0, 0, 255)
+#노란색
+yellow = (255, 255, 0)
 
 n_x = 600
 n_y = 150
@@ -35,5 +55,9 @@ xn3_change = 1
 yn3_change = 0
 xn4_change = 0.25
 yn4_change = 0.25
+kp4 = 0.05
+km4 = -0.05
 ychange = 0
 xchange = 0
+
+speed_score = 30000
