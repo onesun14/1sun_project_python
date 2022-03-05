9bd79bce0fd942aa25d50502730fff144da50652
onesun14 <88918794+onesun14@users.noreply.github.com>
2022-03-05 10:21:17 +0900

    Update main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -19,6 +19,9 @@ import streamlit as st
 #데코레이션, 함수를 꾸며주는건데
 #함수에서 반복적으로 사용되는게 있을때 그거를 모아놓은거라고 생각하지면 될것 같아요.
 
+import pandas as pd
+import numpy as np
+
 if st.button("Click Button"):
     st.write("Hello test")
 
@@ -51,6 +54,36 @@ st.write('you selected', multi_option)
 
 values = st.slider('Select a range of values', 0.0, 100.0, (25.0, 25.0))
 st.write('Values:', values)
+
+st.text_input("enter the input")
+
+st.text_input("enter the number", type="password")
+
+st.number_input("enter the number", 2)
+
+text_area = st.text_area("enter the multi text", "hello")
+if text_area == "?":
+    st.write("????")
+
+st.date_input("data")
+
+st.time_input("Time")
+
+col1, col2, col3 = st.beta_columns(3)
+
+with col1:
+    st.header("A cat?")
+    st.image("./test/dog.png", use_column_width=True)
+
+with col2:
+    st.header("Button")
+    if st.button("Button!!"):
+        st.write("yes")
+
+with col3:
+    st.header('Chart Data')
+    chart_data = pd.DataFrame(np.random.randn(50, 3), columns=["a", "b", "c"])
+    st.bar_chart(chart_data)
 
 @st.cache
 def load_data():
